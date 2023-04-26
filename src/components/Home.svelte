b0f2992146b90bbf2cd2d18e07032b9572443cc8
Jonathan Kim Sarong <100345062+jayekim06@users.noreply.github.com>
2023-04-26 10:08:00 +0800

    Update Home.svelte

--- src/components/Home.svelte
+++ src/components/Home.svelte
@@ -4,6 +4,7 @@
     import units from "../database/getTotalNumberOfUnits";
     import maxOccupants from "../database/maxNumberOfOccupants"
     import { onDestroy } from "svelte";
+    import Carousel from "svelte-carousel";
 
     let up = 0
     let down = 0
@@ -79,7 +80,7 @@
       unsubscribeCapacity()
     });
 
-    let images = [
+   let images = [
       { src: "images/byday.png", alt: "Average Passenger per Day", description: "According to the analysis of our current data trend, it has been observed that Monday records the highest average passenger count among all the days of the week. The visual representation of the data demonstrates that the average number of passengers on Monday amounts to approximately 350 per day." },
       { src: "images/byhour.png", alt: "Average Passenger per Hour", description: "As per the analysis of our current data trend, it has been observed that the time slot of 7:00 to 7:59 AM on Mondays records the highest average passenger count by hour of the day. The visual representation of the data illustrates that during this time slot, the average number of passengers on Mondays amounts to approximately 60 plus per day. This information provides valuable insights into the peak hours of passenger traffic on Mondays, which can be useful for commuters to plan their travel accordingly." },
       { src: "images/byhourtues.png", alt: "Average Passenger per Hour", description: "Based on the analysis of our current data trend, it can be observed that the time window of 8:00 to 8:59 AM on Tuesdays experiences the highest average passenger count per hour of the day. The accompanying visual representation of the data shows that the average number of passengers during this period on Tuesdays is approximately 40 plus per day. These insights into the peak hours of passenger traffic on Tuesdays can prove to be beneficial for commuters in planning their journeys and avoiding congestion during rush hour." },
@@ -88,6 +89,29 @@
       { src: "images/byhourfri.png", alt: "Average Passenger per Hour", description: "Similarly, on Thursdays and Fridays, the time frame of 8:00 to 8:59 AM experiences the highest average passenger count per hour of the day, with approximately 35 plus passengers on average during this period, as illustrated by the accompanying visual representation based on our current data trend. Commuters can use this information to plan their travel and avoid peak congestion during these days and hours." },
       { src: "images/l3.png" , alt: "Average Passenger per Hour", description: "The L300 Van is a reliable and efficient public utility vehicle used for public transportation. It has a spacious and comfortable interior, capable of accommodating up to 16 passengers. Equipped with a powerful and fuel-efficient engine, the L300 Van can handle various road conditions with ease. Its durable and sturdy build ensures safe and secure transportation for commuters. With its accessibility and affordability, the L300 Van is a popular choice for public transportation in urban and rural areas." },
       { src: "images/forecast.png" , alt: "Average Passenger per Hour", description: "This data visualization displays the results of a forecasting analysis performed on the current dataset using the fbprophet algorithm. The forecast extends to 100 days in the future and pertains to the average daily passenger count at a public utility terminal. The graphical representation of the forecasting results allows for easy comprehension and interpretation, enabling stakeholders to make informed decisions based on the anticipated passenger traffic." },
+    ];
+
+    let byhour= [
+      { src: "images/avebyhour.png", alt: "Average Passenger per Hour", description: "As per the analysis of our current data trend, it has been observed that the time slot of 7:00 to 7:59 AM on Mondays records the highest average passenger count by hour of the day. The visual representation of the data illustrates that during this time slot, the average number of passengers on Mondays amounts to approximately 60 plus per day. This information provides valuable insights into the peak hours of passenger traffic on Mondays, which can be useful for commuters to plan their travel accordingly." },
+      { src: "images/byhour.png", alt: "Average Passenger per Hour", description: "As per the analysis of our current data trend, it has been observed that the time slot of 7:00 to 7:59 AM on Mondays records the highest average passenger count by hour of the day. The visual representation of the data illustrates that during this time slot, the average number of passengers on Mondays amounts to approximately 60 plus per day. This information provides valuable insights into the peak hours of passenger traffic on Mondays, which can be useful for commuters to plan their travel accordingly." },
+      { src: "images/byhourtues.png", alt: "Average Passenger per Hour", description: "Based on the analysis of our current data trend, it can be observed that the time window of 8:00 to 8:59 AM on Tuesdays experiences the highest average passenger count per hour of the day. The accompanying visual representation of the data shows that the average number of passengers during this period on Tuesdays is approximately 40 plus per day. These insights into the peak hours of passenger traffic on Tuesdays can prove to be beneficial for commuters in planning their journeys and avoiding congestion during rush hour." },
+      { src: "images/byhourwed.png", alt: "Average Passenger per Hour", description: "On Wednesdays, the time slot of 8:00 to 8:59 AM records the highest average passenger count per hour of the day, with approximately 30 plus passengers on average during this period, as indicated by the accompanying visual representation based on our current data trend. These insights can assist commuters in avoiding peak congestion and planning their travel accordingly." },
+      { src: "images/byhourthurs.png",  alt: "Average Passenger per Hour", description: "Similarly, on Thursdays and Fridays, the time frame of 8:00 to 8:59 AM experiences the highest average passenger count per hour of the day, with approximately 35 plus passengers on average during this period, as illustrated by the accompanying visual representation based on our current data trend. Commuters can use this information to plan their travel and avoid peak congestion during these days and hours." },
+      { src: "images/byhourfri.png", alt: "Average Passenger per Hour", description: "Similarly, on Thursdays and Fridays, the time frame of 8:00 to 8:59 AM experiences the highest average passenger count per hour of the day, with approximately 35 plus passengers on average during this period, as illustrated by the accompanying visual representation based on our current data trend. Commuters can use this information to plan their travel and avoid peak congestion during these days and hours." },
+    ];
+
+    let predbyhour= [
+      { src: "images/avepredbyhour.png", alt: "Average Passenger per Hour", description: "As per the analysis of our current data trend, it has been observed that the time slot of 7:00 to 7:59 AM on Mondays records the highest average passenger count by hour of the day. The visual representation of the data illustrates that during this time slot, the average number of passengers on Mondays amounts to approximately 60 plus per day. This information provides valuable insights into the peak hours of passenger traffic on Mondays, which can be useful for commuters to plan their travel accordingly." },
+      { src: "images/predbyhourmon.png", alt: "Average Passenger per Hour", description: "As per the analysis of our current data trend, it has been observed that the time slot of 7:00 to 7:59 AM on Mondays records the highest average passenger count by hour of the day. The visual representation of the data illustrates that during this time slot, the average number of passengers on Mondays amounts to approximately 60 plus per day. This information provides valuable insights into the peak hours of passenger traffic on Mondays, which can be useful for commuters to plan their travel accordingly." },
+      { src: "images/predbyhourtues.png", alt: "Average Passenger per Hour", description: "Based on the analysis of our current data trend, it can be observed that the time window of 8:00 to 8:59 AM on Tuesdays experiences the highest average passenger count per hour of the day. The accompanying visual representation of the data shows that the average number of passengers during this period on Tuesdays is approximately 40 plus per day. These insights into the peak hours of passenger traffic on Tuesdays can prove to be beneficial for commuters in planning their journeys and avoiding congestion during rush hour." },
+      { src: "images/predbyhourwed.png", alt: "Average Passenger per Hour", description: "On Wednesdays, the time slot of 8:00 to 8:59 AM records the highest average passenger count per hour of the day, with approximately 30 plus passengers on average during this period, as indicated by the accompanying visual representation based on our current data trend. These insights can assist commuters in avoiding peak congestion and planning their travel accordingly." },
+      { src: "images/predbyhourthur.png",  alt: "Average Passenger per Hour", description: "Similarly, on Thursdays and Fridays, the time frame of 8:00 to 8:59 AM experiences the highest average passenger count per hour of the day, with approximately 35 plus passengers on average during this period, as illustrated by the accompanying visual representation based on our current data trend. Commuters can use this information to plan their travel and avoid peak congestion during these days and hours." },
+      { src: "images/predbyhourfri.png", alt: "Average Passenger per Hour", description: "Similarly, on Thursdays and Fridays, the time frame of 8:00 to 8:59 AM experiences the highest average passenger count per hour of the day, with approximately 35 plus passengers on average during this period, as illustrated by the accompanying visual representation based on our current data trend. Commuters can use this information to plan their travel and avoid peak congestion during these days and hours." },
+    ];
+
+    let aveandpredbyday= [
+      { src: "images/byday.png", alt: "Average Passenger per Day", description: "According to the analysis of our current data trend, it has been observed that Monday records the highest average passenger count among all the days of the week. The visual representation of the data demonstrates that the average number of passengers on Monday amounts to approximately 350 per day." },
+      { src: "images/avepredday.png", alt: "Average Passenger per Day", description: "According to the analysis of our current data trend, it has been observed that Monday records the highest average passenger count among all the days of the week. The visual representation of the data demonstrates that the average number of passengers on Monday amounts to approximately 350 per day." },
     ];
   </script>
   <br>
@@ -121,19 +145,44 @@
     </div>
   </div>
   
-  <div class="margin-info">
-    <div class="con-info">
-      {#each images as { src, alt, description }}
-      <div class="container">
-        <img class="image" src="{src}" alt="{alt}">
-        <p class="description">{description}</p>
-      </div>
-      {/each}
+ <br>
+  <p class="mr-2 font-bold text-center">Average Passenger Count By Hour Per Day</p>
+
+<Carousel navigation>
+    {#each byhour as { src, alt }}
+    <div class="container">
+      <img class="image" src="{src}" alt="{alt}">
     </div>
+    {/each}
+</Carousel>
+
+<br><br>
+<p class="mr-2 font-bold text-center">Predicted Average Passenger Count By Hour Per Day</p>
+
+<Carousel navigation>
+  {#each predbyhour as { src, alt }}
+  <div class="container">
+    <img class="image" src="{src}" alt="{alt}">
   </div>
+  {/each}
+</Carousel>
+
+<br><br>
+<p class="mr-2 font-bold text-center">Average and Predicted Average Passenger Count Per Day</p>
+
+<Carousel navigation>
+  {#each aveandpredbyday as { src, alt }}
+  <div class="container">
+    <img class="image" src="{src}" alt="{alt}">
+  </div>
+  {/each}
+</Carousel>
+
   <br>
   <br>
   <br>
+  <br>
+
   <style>
     .heading {
       margin: 30px 20px;
