6e742ca19b5ac2ea8b4a96c5a67ed9ccd7d99bfa
AMlao99 <100349144+AMlao99@users.noreply.github.com>
2024-02-22 21:09:22 +0800

    Update getTotalNumberOfPeople.js back to last update

--- src/database/getTotalNumberOfPeople.js
+++ src/database/getTotalNumberOfPeople.js
@@ -1,34 +1,32 @@
 import { collection, getDocs, onSnapshot } from "firebase/firestore";
 import db from "../auth/firestore"
 import { writable } from "svelte/store";
-
-export let people = writable(0)
-
+export let people = writable([])
 async function getTotalNumberOfPeople() {
     let totalCount = []
-    const querySnapshot = await getDocs(collection(db, "Detections"));
+    const querySnapshot = await getDocs(collection(db, "Total"));
     if(querySnapshot){
         querySnapshot.forEach((doc) => {
             totalCount.push(JSON.parse(JSON.stringify(doc.data())))
         })
     }
-
     return totalCount
 }
-
-
+function setPeople() {
+    getTotalNumberOfPeople().then((out) => {
+        if(out){
+            //console.log(out)
+            people.set(out)
+        }
+    })
+}
 // Set up a realtime listener to update the reactive store whenever there is new data
-onSnapshot(collection(db, "Detections"), (snapshot) => {
-    people.set(snapshot.size); // updates the people count, snapshot.size gives the current number of documents 
-}, error => {
+onSnapshot(collection(db, "Total"), (snapshot) => {
+    if(snapshot){
+        setPeople()
+    }
+}, (error) => {
     console.error(error);
 });
-
-getTotalNumberOfPeople().then((out) => {
-    if(out){
-        people.set(out)
-    }
-});
 setPeople()
-
 export default people
