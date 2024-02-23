3aa56877c43c9399e5ca64be7c49dcb8eef17668
AMlao99 <100349144+AMlao99@users.noreply.github.com>
2024-02-23 21:26:28 +0800

    Update getTotalNumberOfPeople.js update since web app not showing

--- src/database/getTotalNumberOfPeople.js
+++ src/database/getTotalNumberOfPeople.js
@@ -3,8 +3,35 @@ import db from "../auth/firestore"
 import { writable } from "svelte/store";
 
 
-// This store holds the current count of people as a number
+
 export let people = writable(0);
+
+async function getTotalNumberOfPeople() {
+    let totalCount = []
+    const querySnapshot = await getDocs(collection(db, "Total"));
+    if(querySnapshot){
+        querySnapshot.forEach((doc) => {
+           totalCount.push(JSON.parse(JSON.stringify(doc.data())))
+      })
+    }
+    return totalCount
+}
+function setPeople() {
+    getTotalNumberOfPeople().then((out) => {
+        if(out){
+            people.set(out)
+        }
+    })
+}
+
+ //Set up a realtime listener to update the reactive store whenever there is new data
+//onSnapshot(collection(db, "Total"), (snapshot) => {
+//    if(snapshot){
+//        snapshot.docChanges().forEach((change) => {
+//           if (change.type === "modified" || change.type === "added" || change.type === "removed") setPeople()
+//       }
+//   )}
+//});
 
 // Listen to real-time updates on the specific 'current' document in the 'Detections' collection
 onSnapshot(doc(db, "Detections", "current"), (documentSnapshot) => {
@@ -17,40 +44,6 @@ onSnapshot(doc(db, "Detections", "current"), (documentSnapshot) => {
     }
 });
 
-export default people;
-
-
-
-
-//export let people = writable([])
-
-//async function getTotalNumberOfPeople() {
-//    let totalCount = []
-//    const querySnapshot = await getDocs(collection(db, "Total"));
-//    if(querySnapshot){
-//        querySnapshot.forEach((doc) => {
-//            totalCount.push(JSON.parse(JSON.stringify(doc.data())))
- //       })
-//    }
-//    return totalCount
-//}
-//function setPeople() {
-//    getTotalNumberOfPeople().then((out) => {
-//        if(out){
-//            people.set(out)
-//        }
-//    })
-//}
-
-// Set up a realtime listener to update the reactive store whenever there is new data
-//onSnapshot(collection(db, "Total"), (snapshot) => {
-//    if(snapshot){
-//        snapshot.docChanges().forEach((change) => {
- //           if (change.type === "modified" || change.type === "added" || change.type === "removed") setPeople()
- //       }
- //   )}
-//});
-
 //setPeople()
 
-//export default people
+export default people
