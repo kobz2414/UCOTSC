8778ae94577c0baf0d2317ba4bff9e119c91dc99
AMlao99 <100349144+AMlao99@users.noreply.github.com>
2024-02-23 17:43:42 +0800

    Update getTotalNumberOfPeople.js from Detections to Total

--- src/database/getTotalNumberOfPeople.js
+++ src/database/getTotalNumberOfPeople.js
@@ -7,7 +7,7 @@ export let people = writable([])
 
 async function getTotalNumberOfPeople() {
     let totalCount = []
-    const querySnapshot = await getDocs(collection(db, "Detections"));
+    const querySnapshot = await getDocs(collection(db, "Total"));
     if(querySnapshot){
         querySnapshot.forEach((doc) => {
             totalCount.push(JSON.parse(JSON.stringify(doc.data())))
@@ -24,7 +24,7 @@ function setPeople() {
     })
 }
 // Set up a realtime listener to update the reactive store whenever there is new data
-onSnapshot(collection(db, "Detections"), (snapshot) => {
+onSnapshot(collection(db, "Total"), (snapshot) => {
     if(snapshot){
         snapshot.docChanges().forEach((change) => {
             if (change.type === "modified") setPeople()
