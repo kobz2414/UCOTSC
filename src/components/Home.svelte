8fb886bfdac26e4db6697c28c64742e8ff3d4100
AMlao99 <100349144+AMlao99@users.noreply.github.com>
2024-02-29 05:13:00 +0800

    Update Home.svelte back to default

--- src/components/Home.svelte
+++ src/components/Home.svelte
@@ -85,6 +85,82 @@
     },
   ];
 
+  const byhour = [
+    {
+      src: "images/avebyhour.png",
+      alt: "Average Passenger per Hour",
+      description:
+        "As per the analysis of our current data trend, it has been observed that the time slot of 7:00 to 7:59 AM on Mondays records the highest average passenger count by hour of the day. The visual representation of the data illustrates that during this time slot, the average number of passengers on Mondays amounts to approximately 60 plus per day. This information provides valuable insights into the peak hours of passenger traffic on Mondays, which can be useful for commuters to plan their travel accordingly.",
+    },
+    {
+      src: "images/byhour.png",
+      alt: "Average Passenger per Hour",
+      description:
+        "As per the analysis of our current data trend, it has been observed that the time slot of 7:00 to 7:59 AM on Mondays records the highest average passenger count by hour of the day. The visual representation of the data illustrates that during this time slot, the average number of passengers on Mondays amounts to approximately 60 plus per day. This information provides valuable insights into the peak hours of passenger traffic on Mondays, which can be useful for commuters to plan their travel accordingly.",
+    },
+    {
+      src: "images/byhourtues.png",
+      alt: "Average Passenger per Hour",
+      description:
+        "Based on the analysis of our current data trend, it can be observed that the time window of 8:00 to 8:59 AM on Tuesdays experiences the highest average passenger count per hour of the day. The accompanying visual representation of the data shows that the average number of passengers during this period on Tuesdays is approximately 40 plus per day. These insights into the peak hours of passenger traffic on Tuesdays can prove to be beneficial for commuters in planning their journeys and avoiding congestion during rush hour.",
+    },
+    {
+      src: "images/byhourwed.png",
+      alt: "Average Passenger per Hour",
+      description:
+        "On Wednesdays, the time slot of 8:00 to 8:59 AM records the highest average passenger count per hour of the day, with approximately 30 plus passengers on average during this period, as indicated by the accompanying visual representation based on our current data trend. These insights can assist commuters in avoiding peak congestion and planning their travel accordingly.",
+    },
+    {
+      src: "images/byhourthurs.png",
+      alt: "Average Passenger per Hour",
+      description:
+        "Similarly, on Thursdays and Fridays, the time frame of 8:00 to 8:59 AM experiences the highest average passenger count per hour of the day, with approximately 35 plus passengers on average during this period, as illustrated by the accompanying visual representation based on our current data trend. Commuters can use this information to plan their travel and avoid peak congestion during these days and hours.",
+    },
+    {
+      src: "images/byhourfri.png",
+      alt: "Average Passenger per Hour",
+      description:
+        "Similarly, on Thursdays and Fridays, the time frame of 8:00 to 8:59 AM experiences the highest average passenger count per hour of the day, with approximately 35 plus passengers on average during this period, as illustrated by the accompanying visual representation based on our current data trend. Commuters can use this information to plan their travel and avoid peak congestion during these days and hours.",
+    },
+  ];
+  const predbyhour = [
+    {
+      src: "images/avepredbyhour.png",
+      alt: "Average Passenger per Hour",
+      description:
+        "As per the analysis of our current data trend, it has been observed that the time slot of 7:00 to 7:59 AM on Mondays records the highest average passenger count by hour of the day. The visual representation of the data illustrates that during this time slot, the average number of passengers on Mondays amounts to approximately 60 plus per day. This information provides valuable insights into the peak hours of passenger traffic on Mondays, which can be useful for commuters to plan their travel accordingly.",
+    },
+    {
+      src: "images/predbyhourmon.png",
+      alt: "Average Passenger per Hour",
+      description:
+        "As per the analysis of our current data trend, it has been observed that the time slot of 7:00 to 7:59 AM on Mondays records the highest average passenger count by hour of the day. The visual representation of the data illustrates that during this time slot, the average number of passengers on Mondays amounts to approximately 60 plus per day. This information provides valuable insights into the peak hours of passenger traffic on Mondays, which can be useful for commuters to plan their travel accordingly.",
+    },
+    {
+      src: "images/predbyhourtues.png",
+      alt: "Average Passenger per Hour",
+      description:
+        "Based on the analysis of our current data trend, it can be observed that the time window of 8:00 to 8:59 AM on Tuesdays experiences the highest average passenger count per hour of the day. The accompanying visual representation of the data shows that the average number of passengers during this period on Tuesdays is approximately 40 plus per day. These insights into the peak hours of passenger traffic on Tuesdays can prove to be beneficial for commuters in planning their journeys and avoiding congestion during rush hour.",
+    },
+    {
+      src: "images/predbyhourwed.png",
+      alt: "Average Passenger per Hour",
+      description:
+        "On Wednesdays, the time slot of 8:00 to 8:59 AM records the highest average passenger count per hour of the day, with approximately 30 plus passengers on average during this period, as indicated by the accompanying visual representation based on our current data trend. These insights can assist commuters in avoiding peak congestion and planning their travel accordingly.",
+    },
+    {
+      src: "images/predbyhourthur.png",
+      alt: "Average Passenger per Hour",
+      description:
+        "Similarly, on Thursdays and Fridays, the time frame of 8:00 to 8:59 AM experiences the highest average passenger count per hour of the day, with approximately 35 plus passengers on average during this period, as illustrated by the accompanying visual representation based on our current data trend. Commuters can use this information to plan their travel and avoid peak congestion during these days and hours.",
+    },
+    {
+      src: "images/predbyhourfri.png",
+      alt: "Average Passenger per Hour",
+      description:
+        "Similarly, on Thursdays and Fridays, the time frame of 8:00 to 8:59 AM experiences the highest average passenger count per hour of the day, with approximately 35 plus passengers on average during this period, as illustrated by the accompanying visual representation based on our current data trend. Commuters can use this information to plan their travel and avoid peak congestion during these days and hours.",
+    },
+  ];
 
   const aveandpredbyday = [
     {
