5fd07500c7038e788e303832fcadcd88d7ed2d67
AMlao99 <100349144+AMlao99@users.noreply.github.com>
2024-03-07 17:08:11 +0000

    Add files via upload

---
+++ src/pages/_layout.svelte
@@ -0,0 +1,36 @@
+<script>
+    import { Router, Route, navigate } from 'svelte-routing';
+    import { Nav } from '../components'
+    import { Footer } from '../components'
+    import { FooterLogin } from '../components'
+    import { userlog } from './../auth/index.js';
+    import { Home, Alert, About, Units, AlertLogin, HomeLogin } from '../components';
+    import { onMount } from 'svelte';
+
+    onMount(() => {
+        document.title = 'United Calinan Operators Transport Service Cooperative';
+        navigate("/Home");
+        setTimeout(() => {
+      navigate("/");
+    }, 100);
+    });
+
+</script> 
+
+<Nav />
+
+<Router>
+    <Route path="/Home" component={Home} default/>
+    <Route path="/Units" component={Units} />
+    <Route path="/Alert" component={Alert} />
+    <Route path="/About" component={About} />
+    <Route path="/UnitsLogin" component={HomeLogin} />
+    <Route path="/AlertLogin" component={AlertLogin} />
+    <div>
+        {#if $userlog} 
+        <FooterLogin/>
+        {:else}
+        <Footer/>
+        {/if}
+    </div>
+</Router>
