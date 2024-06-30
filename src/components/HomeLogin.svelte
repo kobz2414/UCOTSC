f48c42b527210b6f465bcbf8ffe974b982751d26
AMlao99 <100349144+AMlao99@users.noreply.github.com>
2024-06-30 16:55:34 +0800

    Update HomeLogin.svelte updated updateChipsColor

--- src/components/HomeLogin.svelte
+++ src/components/HomeLogin.svelte
@@ -18,7 +18,7 @@
     appId: "1:460072388182:web:bb1940dc98696e405c9df7"
   };
 
-let chipImageSrc = "";
+$: chipImageSrc = "";
 // Initialize Firebase
   const app = initializeApp(firebaseConfig);
   const db = getFirestore(app);
@@ -31,7 +31,6 @@ let chipImageSrc = "";
 
     chipImageSrc = `images/${color}-chip.png`;
   }
-
   // Function to reset the chip color in Firestore
   async function resetChipColor() {
     const chipColorRef = doc(db, 'chipColors', 'currentColor');
