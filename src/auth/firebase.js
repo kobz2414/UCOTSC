d7a1447eeb978b1eb482d53c25deafe631cf1281
AMlao99 <100349144+AMlao99@users.noreply.github.com>
2024-07-01 12:05:02 +0800

    Update firebase.js again ambot nalang

--- src/auth/firebase.js
+++ src/auth/firebase.js
@@ -1,15 +1,7 @@
 import { initializeApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
 import { getAuth } from 'firebase/auth';
-
-const firebaseConfig = {
-  apiKey: "AIzaSyDZKN4wBLlmynAM20uS_Dk9jr64QQNKlKo",
-  authDomain: "ucotsc-new.firebaseapp.com",
-  projectId: "ucotsc-new",
-  storageBucket: "ucotsc-new.appspot.com",
-  messagingSenderId: "460072388182",
-  appId: "1:460072388182:web:bb1940dc98696e405c9df7"
-};
+import { firebaseConfig } from './config.js';
 
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
