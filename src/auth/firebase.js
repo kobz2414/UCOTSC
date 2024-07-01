09f80c51ee0a67c1cfa029c29d387da7954a618d
AMlao99 <100349144+AMlao99@users.noreply.github.com>
2024-07-01 12:00:18 +0800

    Update firebase.js with pepito code

--- src/auth/firebase.js
+++ src/auth/firebase.js
@@ -1,8 +1,18 @@
-import firebase from 'firebase/compat/app'
-import 'firebase/compat/firestore'
-import 'firebase/compat/auth'
-import { firebaseConfig } from './config.js'
+import { initializeApp } from 'firebase/app';
+import { getFirestore } from 'firebase/firestore';
+import { getAuth } from 'firebase/auth';
 
-firebase.initializeApp(firebaseConfig)
+const firebaseConfig = {
+  apiKey: "AIzaSyDZKN4wBLlmynAM20uS_Dk9jr64QQNKlKo",
+  authDomain: "ucotsc-new.firebaseapp.com",
+  projectId: "ucotsc-new",
+  storageBucket: "ucotsc-new.appspot.com",
+  messagingSenderId: "460072388182",
+  appId: "1:460072388182:web:bb1940dc98696e405c9df7"
+};
 
-export default firebase;
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+const auth = getAuth(app);
+
+export { app, db, auth };
